f09e13ea6281e511a4430d38df0c35f0340b8095
PeterZhuchenko <peterzhuchenko@gmail.com>
2018-07-17 20:56:27 +0300

    FIRST COMMIT

---
+++ GitLab/Controllers/GigsController.cs
@@ -0,0 +1,57 @@
+﻿using GitLab.Models;
+using GitLab.Models.GitModels;
+using GitLab.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitLab.Controllers
+{
+    public class GigsController : Controller
+    {
+        ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("Create", viewModel);
+            }
+                        
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue,
+                DateTime = viewModel.GetDateTime()
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ GitLab/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitLab.Startup))]
+namespace GitLab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
