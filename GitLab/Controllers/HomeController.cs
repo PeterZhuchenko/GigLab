e690ff6afc6b22250c3e7d7d90461bbb9b5e809f
PeterZhuchenko <peterzhuchenko@gmail.com>
2018-08-05 10:42:13 +0300

    UI improvements

--- GitLab/Controllers/HomeController.cs
+++ GitLab/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GitLab.Models;
+using System.Data.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,20 @@ namespace GitLab.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .Where(g => g.DateTime > DateTime.Now);
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
