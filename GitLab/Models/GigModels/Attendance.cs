9c8e0025e6191c5c392f91da05b2fa6e7ea48ab2
PeterZhuchenko <peterzhuchenko@gmail.com>
2018-08-14 19:46:35 +0300

    Attendance Added

---
+++ GitLab/Models/GigModels/Attendance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Attendance
+{
+	public Attendance()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
